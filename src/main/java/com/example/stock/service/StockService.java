c65e1e865a568161b692f60d80836bbef51f5f84
양경희 <khyang@snack24h.com>
2023-02-27 23:02:04 +0900

    fix: 메서드에 Synchronized 사용하여 동시성 이슈 해결

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -3,7 +3,6 @@ package com.example.stock.service;
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class StockService {
@@ -14,10 +13,10 @@ public class StockService {
     this.stockRepository = stockRepository;
   }
 
-  @Transactional
-  public void decrease(Long id, Long quantity) {
-    Stock stock = stockRepository.findById(id).orElseThrow();
-    stock.decrease(quantity);
-    stockRepository.save(stock);
-  }
+//  @Transactional
+public synchronized void decrease(Long id, Long quantity) {
+  Stock stock = stockRepository.findById(id).orElseThrow();
+  stock.decrease(quantity);
+  stockRepository.save(stock);
+}
 }
